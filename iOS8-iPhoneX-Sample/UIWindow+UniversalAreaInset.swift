d226d45cc7da882276b4557e742bfb990327bb3d
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-04-25 10:15:27 +0700

    Initial commit for UIWindow+UniversalAreaInset

---
+++ iOS8-iPhoneX-Sample/UIWindow+UniversalAreaInset.swift
@@ -0,0 +1,25 @@
+//
+//  UIWindow+UniversalAreaInset.swift
+//  iOS8-iPhoneX-Sample
+//
+//  Created by Ricardo Pramana Suranta on 25/04/18.
+//  Copyright © 2018 Ice House. All rights reserved.
+//
+
+import UIKit
+
+extension UIWindow {
+	
+	/**
+	Returns `safeAreaInsets` for iOS 11 and above, and zero edge inset for lower.
+	*/
+	var universalAreaInsets: UIEdgeInsets {
+		
+		if #available(iOS 11, *) {
+			return self.safeAreaInsets
+		} else {
+			return UIEdgeInsets.zero
+		}
+	}
+	
+}
