2357b1bbf4fff17fdd362ad11362d629896ef9a0
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-04-25 10:05:06 +0700

    Initial commit for SampleViewController

---
+++ iOS8-iPhoneX-Sample/SampleViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SampleViewController.swift
+//  iOS8-iPhoneX-Sample
+//
+//  Created by Ricardo Pramana Suranta on 25/04/18.
+//  Copyright © 2018 Ice House. All rights reserved.
+//
+
+import UIKit
+
+final class SampleViewController: UIViewController {
+
+	@IBOutlet private weak var bottomConstraint: NSLayoutConstraint!
+	@IBOutlet private weak var topConstraint: NSLayoutConstraint!
+	@IBOutlet private weak var trailingConstraint: NSLayoutConstraint!
+	@IBOutlet private weak var leadingConstraint: NSLayoutConstraint!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
