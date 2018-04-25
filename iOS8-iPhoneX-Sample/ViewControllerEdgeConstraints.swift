15835b9dfd78169058974854a8c29837f4d76245
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-04-25 11:24:03 +0700

    Initial commit for ViewControllerEdgeConstraints

---
+++ iOS8-iPhoneX-Sample/ViewControllerEdgeConstraints.swift
@@ -0,0 +1,28 @@
+//
+//  ViewControllerEdgeConstraints.swift
+//  iOS8-iPhoneX-Sample
+//
+//  Created by Ricardo Pramana Suranta on 25/04/18.
+//  Copyright © 2018 Ice House. All rights reserved.
+//
+
+import UIKit
+
+struct ViewControllerEdgeConstraints {
+	
+	let top: NSLayoutConstraint?
+	let bottom: NSLayoutConstraint?
+	let leading: NSLayoutConstraint?
+	let trailing: NSLayoutConstraint?
+	
+	/**
+	Configures the constraints based on passet `edgeInsets`.
+	*/
+	func configure(edgeInsets: UIEdgeInsets) {
+		
+		top?.constant = edgeInsets.top
+		bottom?.constant = edgeInsets.bottom
+		leading?.constant = edgeInsets.left
+		trailing?.constant = edgeInsets.right
+	}
+}
