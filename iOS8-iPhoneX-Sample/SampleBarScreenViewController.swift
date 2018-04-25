67024d367354180721735e4e6bd300e6eeaaffcf
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2018-04-25 11:03:28 +0700

    Initial commit for SampleBarScreenViewController

---
+++ iOS8-iPhoneX-Sample/SampleBarScreenViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SampleBarScreenViewController.swift
+//  iOS8-iPhoneX-Sample
+//
+//  Created by Ricardo Pramana Suranta on 25/04/18.
+//  Copyright © 2018 Ice House. All rights reserved.
+//
+
+import UIKit
+
+final class SampleBarScreenViewController: BaseViewController {
+	
+	@IBOutlet private weak var topConstraint: NSLayoutConstraint!
+	@IBOutlet private weak var bottomConstraint: NSLayoutConstraint!
+	@IBOutlet private weak var leadingConstraint: NSLayoutConstraint!
+	@IBOutlet private weak var trailingConstraint: NSLayoutConstraint!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
